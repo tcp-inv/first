9a518f02676cfa967228fdbab13fadc4ef065a60
tcp <thongchai.te@invitracehealth.com>
2024-07-16 10:36:12 +0700

    Scound

---
+++ perm.js
@@ -0,0 +1,9 @@
+// Sample JavaScript file (script.js)
+
+// Function to greet the user
+function greet(name) {
+    console.log(`Hello, ${name}! Welcome to JavaScript.`);
+}
+
+// Call the greet function
+greet("John Doe");
